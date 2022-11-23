3e5fa39b710bb4e9c73f723b4d160a4b802e1a5b
Kevin Whitley <krwhitley@gmail.com>
2022-11-23 16:23:08 -0600

    released v0.0.4 - simple splash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,33 @@
 </svelte:head>
 
 <!-- MARKUP -->
-<h1>Main Page</h1>
+<section class="splash">
+  <h1>
+    <small>the wedding of</small>
+    Aimee &amp; Kevin
+  </h1>
+</section>
 
 <!-- STYLES -->
 <style lang="scss">
+  .splash {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 60vh;
+  }
 
+  h1 {
+    white-space: pre-line;
+    text-align: center;
+    letter-spacing: 0;
+    font-size: 6em;
+  }
+
+  small {
+    font-size: 0.4em;
+    display: block;
+    margin-bottom: -2.2em;
+  }
 </style>
